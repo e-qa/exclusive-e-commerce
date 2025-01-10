fe43093bf83d7833f52455406acaa06d3464fe7e
e-qa <eliqarayev835@gmail.com>
2025-01-10 11:15:01 +0400

    style slider items

--- components/hero/FeaturedCarousel.tsx
+++ components/hero/FeaturedCarousel.tsx
@@ -1,42 +1,77 @@
 'use client';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-// import required modules
 import { Navigation } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
+
 const FeaturedCarousel = () => {
   return (
-    <div className="border-2 mb-10">
+    <div className="border-2 mb-12">
       <Swiper
+        pagination={{ clickable: true }}
         navigation={false}
-        modules={[Navigation]}
-        className="mySwiper h-[590px]"
+        modules={[Navigation, Pagination]}
+        className="mySwiper h-[36.3rem]"
       >
         <SwiperSlide>
-          <div className="flex justify-center flex-col items-center h-full gap-10">
+          <div className="flex justify-center flex-col items-center h-full gap-5 px-20">
+            <div className="w-52 h-52 rounded-full bg-primary text-white relative  font-bold">
+              <div className="w-1 h-10 bg-[#e5e7eb] absolute -top-10 left-24"></div>
+              <div className="w-10 h-10 rounded-full absolute top-4 left-20 bg-white"></div>
+              <span className="absolute bottom-20 left-[74px] font-bold text-2xl">
+                SALE
+              </span>
+            </div>
             <h4 className="text-5xl">45% OFF</h4>
+            <p className="text-center">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
+              earum. Adipisci molestiae velit at. Labore architecto eaque
+              tenetur est soluta numquam mollitia
+            </p>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 lg:py-2 text-sm lg:text-base px-4 rounded">
+              View Sale
+            </button>
+          </div>
+        </SwiperSlide>
 
+        <SwiperSlide>
+          <div className="flex justify-center flex-col items-center h-full gap-5 px-20">
+            <div className="w-52 h-52 rounded-full bg-primary text-white relative  font-bold">
+              <div className="w-1 h-10 bg-[#e5e7eb] absolute -top-10 left-24"></div>
+              <div className="w-10 h-10 rounded-full absolute top-4 left-20 bg-white"></div>
+              <span className="absolute bottom-20 left-[74px] font-bold text-2xl">
+                SALE
+              </span>
+            </div>
+            <h4 className="text-5xl">45% OFF</h4>
+            <p className="text-center">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
+              earum. Adipisci molestiae velit at. Labore architecto eaque
+              tenetur est soluta numquam mollitia
+            </p>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 lg:py-2 text-sm lg:text-base px-4 rounded">
               View Sale
             </button>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="flex justify-center flex-col items-center h-full gap-10">
+          <div className="flex justify-center flex-col items-center h-full gap-5 px-20">
+            <div className="w-52 h-52 rounded-full bg-primary text-white relative  font-bold">
+              <div className="w-1 h-10 bg-[#e5e7eb] absolute -top-10 left-24"></div>
+              <div className="w-10 h-10 rounded-full absolute top-4 left-20 bg-white"></div>
+              <span className="absolute bottom-20 left-[74px] font-bold text-2xl">
+                SALE
+              </span>
+            </div>
             <h4 className="text-5xl">45% OFF</h4>
-
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 lg:py-2 text-sm lg:text-base px-4 rounded">
-              View Sale
-            </button>
-          </div>
-        </SwiperSlide>{' '}
-        <SwiperSlide>
-          <div className="flex justify-center flex-col items-center h-full gap-10">
-            <h4 className="text-5xl">45% OFF</h4>
-
+            <p className="text-center">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
+              earum. Adipisci molestiae velit at. Labore architecto eaque
+              tenetur est soluta numquam mollitia
+            </p>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 lg:py-2 text-sm lg:text-base px-4 rounded">
               View Sale
             </button>
